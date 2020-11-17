69fa2ae686727f7eaf7d3e7db350914fcedc383f
BubbleMonkey-blip <beckdp01@gmail.com>
2020-11-16 20:37:19 -0500

    finished files

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-let myLibrary = [];
+const firebaseConfig = {
+  apiKey: "AIzaSyB85KXXoGvQ1lbSORpMJ35GsbO1-imamig",
+  authDomain: "my-library-7fcb9.firebaseapp.com",
+  databaseURL: "https://my-library-7fcb9.firebaseio.com",
+  projectId: "my-library-7fcb9",
+  storageBucket: "my-library-7fcb9.appspot.com",
+  messagingSenderId: "764998864378",
+  appId: "1:764998864378:web:6c45a128c4663a0175a843",
+  measurementId: "G-V5MG7HEZDW"
+  };
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 function Book (title, author, pages, read) {
   this.title = title;
@@ -7,61 +19,67 @@ function Book (title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary (title, author, pages, read) {
+const addBookToLibrary = (title, author, pages, read) => {
   const book = new Book(title, author, pages, read);
-  myLibrary.push(book);
+  firebase.database().ref(title).set(book);
 }
 
-function updateDisplay () {
-  document.getElementById("libraryDisplay").innerHTML = "";
-  for (let i = 0; i < myLibrary.length; i++) {
-    let bookCard = document.createElement("div");
-    bookCard.className = "bookCard";
-    let titleLine = document.createElement("p");
-    titleLine.innerHTML = "Title: " + myLibrary[i].title;
-    bookCard.appendChild(titleLine);
-    let authorLine = document.createElement("p");
-    authorLine.innerHTML = "Author: " + myLibrary[i].author;
-    bookCard.appendChild(authorLine);
-    let pagesLine = document.createElement("p");
-    pagesLine.innerHTML = "Number of Pages: " + myLibrary[i].pages;
-    bookCard.appendChild(pagesLine);
-    let readLine = document.createElement("p");
-    readLine.innerHTML = "Read?";
-    bookCard.appendChild(readLine);
-    let readBox = document.createElement("input");
-    readBox.type = "checkbox";
-    if (myLibrary[i].read = true) {
-      readBox.checked = true;
-    }
-    readBox.onclick = function () {
-      myLibrary[i].read = !myLibrary[i].read;
-    }
-    readLine.appendChild(readBox);
-    let deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "Delete";
-    deleteButton.style.margin = "0 auto";
-    deleteButton.style.display = "block";
-    deleteButton.style.backgroundColor = "#560C03";
-    deleteButton.style.color= "white";
-    deleteButton.style.borderColor = "black";
-    deleteButton.onclick = function () {
-      bookCard.remove();
-      myLibrary.splice(i, 1);
-    };
-    bookCard.appendChild(deleteButton);
-    document.getElementById("libraryDisplay").appendChild(bookCard);
-  }
+const updateDisplay = () => {
+  let libraryRef = firebase.database().ref();
+  libraryRef.on('value', (snapshot)=> {
+    document.getElementById("libraryDisplay").innerHTML = "";
+    for (const prop in snapshot.val()){
+      let bookCard = document.createElement("div");
+      bookCard.className = "bookCard";
+      let titleLine = document.createElement("p");
+      titleLine.innerHTML = "Title: " + snapshot.val()[prop].title;
+      bookCard.appendChild(titleLine);
+      let authorLine = document.createElement("p");
+      authorLine.innerHTML = "Author: " + snapshot.val()[prop].author;
+      bookCard.appendChild(authorLine);
+      let pagesLine = document.createElement("p");
+      pagesLine.innerHTML = "Number of Pages: " + snapshot.val()[prop].pages;
+      bookCard.appendChild(pagesLine);
+      let readLine = document.createElement("p");
+      readLine.innerHTML = "Read?";
+      bookCard.appendChild(readLine);
+      let readBox = document.createElement("input");
+      readBox.type = "checkbox";
+      let valOf = snapshot.val()[prop].read;
+      readBox.checked = valOf;
+      readBox.onclick = function() {
+       firebase.database().ref(prop + "/read").set(!valOf);
+      };
+      readLine.appendChild(readBox);
+      let deleteButton = document.createElement("button");
+      deleteButton.innerHTML = "Delete";
+      deleteButton.style.margin = "0 auto";
+      deleteButton.style.display = "block";
+      deleteButton.style.backgroundColor = "#560C03";
+      deleteButton.style.color= "white";
+      deleteButton.style.borderColor = "black";
+      deleteButton.onclick = function () {
+        bookCard.remove();
+        firebase.database().ref(prop).remove();
+      };
+      bookCard.appendChild(deleteButton);
+      document.getElementById("libraryDisplay").appendChild(bookCard);
+    } 
+  });
 }
 
-function openForm() {
+const openForm = () => {
   document.getElementById("popupForm").style.display = "block";
   document.getElementById("btnOpen").style.opacity = "5%";
   document.getElementById("libraryDisplay").style.opacity = "5%";
 }
 
-function closeForm() {
+const closeForm = () => {
   document.getElementById("popupForm").style.display = "none";
   document.getElementById("btnOpen").style.opacity = "100%";
   document.getElementById("libraryDisplay").style.opacity = "100%";
+}
+
+const resetForm = () => {
+  document.getElementById("form").reset();
 }
