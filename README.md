db3022a24414a10cdb80dcb930672e2fea3eff40
Dan Beck <beckdp01@gmail.com>
2022-09-28 12:37:39 -0400

    refactor code and remove firebase backend

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # library_app
-A simple web app for storing books you have read
+A simple web app for storing books in your library. Use the button in the right hand corner to add a new card for a book in your library. Currently, the app does not have a storage system built in, but this may be added later via local storage or firebase.
+
+The javascript code is organized using object constructor function. This app was built as I worked through the Odin Project curriculum and learning about methods of organizing your javascript code..
--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyB85KXXoGvQ1lbSORpMJ35GsbO1-imamig",
-  authDomain: "my-library-7fcb9.firebaseapp.com",
-  databaseURL: "https://my-library-7fcb9.firebaseio.com",
-  projectId: "my-library-7fcb9",
-  storageBucket: "my-library-7fcb9.appspot.com",
-  messagingSenderId: "764998864378",
-  appId: "1:764998864378:web:6c45a128c4663a0175a843",
-  measurementId: "G-V5MG7HEZDW"
-  };
+let myLibrary = [];
 
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+const openButton = document.getElementById("openButton");
+openButton.addEventListener("click", openForm);
+
+const closeButton = document.getElementById("closeButton")
+closeButton.addEventListener("click", closeForm);
+
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", closeForm);
+submitButton.addEventListener("click", resetForm);
+submitButton.addEventListener("click", updateDisplay);
+
 
 function Book (title, author, pages, read) {
   this.title = title;
@@ -19,67 +19,59 @@ function Book (title, author, pages, read) {
   this.read = read;
 }
 
-const addBookToLibrary = (title, author, pages, read) => {
+function addBookToLibrary(title, author, pages, read) {
   const book = new Book(title, author, pages, read);
-  firebase.database().ref(title).set(book);
+  myLibrary.push(book);
 }
 
-const updateDisplay = () => {
-  let libraryRef = firebase.database().ref();
-  libraryRef.on('value', (snapshot)=> {
-    document.getElementById("libraryDisplay").innerHTML = "";
-    for (const prop in snapshot.val()){
-      let bookCard = document.createElement("div");
-      bookCard.className = "bookCard";
-      let titleLine = document.createElement("p");
-      titleLine.innerHTML = "Title: " + snapshot.val()[prop].title;
-      bookCard.appendChild(titleLine);
-      let authorLine = document.createElement("p");
-      authorLine.innerHTML = "Author: " + snapshot.val()[prop].author;
-      bookCard.appendChild(authorLine);
-      let pagesLine = document.createElement("p");
-      pagesLine.innerHTML = "Number of Pages: " + snapshot.val()[prop].pages;
-      bookCard.appendChild(pagesLine);
-      let readLine = document.createElement("p");
-      readLine.innerHTML = "Read?";
-      bookCard.appendChild(readLine);
-      let readBox = document.createElement("input");
-      readBox.type = "checkbox";
-      let valOf = snapshot.val()[prop].read;
-      readBox.checked = valOf;
-      readBox.onclick = function() {
-       firebase.database().ref(prop + "/read").set(!valOf);
+function updateDisplay() {
+  document.getElementById("libraryDisplay").innerHTML = "";
+  myLibrary.forEach((e) => {
+    let bookCard = document.createElement("div");
+    bookCard.className = "bookCard";
+    let titleLine = document.createElement("p");
+    titleLine.innerHTML = "Title: " + e.title;
+    bookCard.appendChild(titleLine);
+    let authorLine = document.createElement("p");
+    authorLine.innerHTML = "Author: " + e.author;
+    bookCard.appendChild(authorLine);
+    let pagesLine = document.createElement("p");
+    pagesLine.innerHTML = "Number of Pages: " + e.pages;
+    bookCard.appendChild(pagesLine);
+    let readLine = document.createElement("p");
+    readLine.innerHTML = "Read?";
+    bookCard.appendChild(readLine);
+    let readBox = document.createElement("input");
+    readBox.type = "checkbox";
+    readBox.checked = e.read;
+    readBox.onclick = function() {  
+      if (e.read === true) {
+        e.read = false;
+      } else {e.read = true};
       };
-      readLine.appendChild(readBox);
-      let deleteButton = document.createElement("button");
-      deleteButton.innerHTML = "Delete";
-      deleteButton.style.margin = "0 auto";
-      deleteButton.style.display = "block";
-      deleteButton.style.backgroundColor = "#560C03";
-      deleteButton.style.color= "white";
-      deleteButton.style.borderColor = "black";
-      deleteButton.onclick = function () {
-        bookCard.remove();
-        firebase.database().ref(prop).remove();
-      };
-      bookCard.appendChild(deleteButton);
-      document.getElementById("libraryDisplay").appendChild(bookCard);
-    } 
+    readLine.appendChild(readBox);
+    let deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "Delete";
+    deleteButton.setAttribute("class", "deleteButton");
+    deleteButton.onclick = function () {
+      bookCard.remove();
+      myLibrary.splice(myLibrary.indexOf(e), 1);
+    };
+    bookCard.appendChild(deleteButton);
+    document.getElementById("libraryDisplay").appendChild(bookCard);
   });
-}
+};
 
-const openForm = () => {
+function openForm() {
   document.getElementById("popupForm").style.display = "block";
-  document.getElementById("btnOpen").style.opacity = "5%";
-  document.getElementById("libraryDisplay").style.opacity = "5%";
+  document.getElementById("libraryDisplay").style.opacity = "25%";
 }
 
-const closeForm = () => {
+function closeForm() {
   document.getElementById("popupForm").style.display = "none";
-  document.getElementById("btnOpen").style.opacity = "100%";
   document.getElementById("libraryDisplay").style.opacity = "100%";
 }
 
-const resetForm = () => {
+function resetForm() {
   document.getElementById("form").reset();
 }
